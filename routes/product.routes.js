74c74f5829432380ffbcdfc91e701c7796c85de2
MohdZaid96 <i.m.mdz.1996@gmail.com>
2023-09-19 16:52:08 +0530

    full

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -17,9 +17,7 @@ productRouter.get("/",async(req,res)=>{
       const totalBlogs = await ProductModel.countDocuments();
   
       res.send({
-        data,
-        currentPage: page,
-        totalPages: Math.ceil(totalBlogs / limit),
+        data
       });
     } catch (err) {
       res.status(500).json({ error: 'Internal server error' });
@@ -30,10 +28,17 @@ productRouter.get("/",async(req,res)=>{
 
 productRouter.get("/sort",async(req,res)=>{
     const sortField = req.query.sort || 'createdAt'; // Default to sorting by createdAt
-    const sortOrder = req.query.order || 'desc'; // Default to descending order
+    const sortOrder = req.query.order || 'asc'; // Default to descending order
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 6;
+    console.log(sortField);
+    console.log(sortOrder);
+    console.log(page);
+    console.log(limit)
   
     try {
-      const data = await ProductModel.find().sort({ [sortField]: sortOrder }).exec();
+      const data = await ProductModel.find().sort({ [sortField]: sortOrder }).skip((page - 1) * limit)
+      .limit(limit).exec();
   
       res.send({ data });
     } catch (err) {
@@ -41,13 +46,30 @@ productRouter.get("/sort",async(req,res)=>{
     }
   });
 
-  productRouter.get("//search",async(req,res)=>{
+  productRouter.get("/search",async(req,res)=>{
     const query = req.query.q;
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 6;
   
     try {
       const product = await ProductModel.find({
         $or: [{ title: { $regex: query, $options: 'i' } }, { content: { $regex: query, $options: 'i' } }],
-      }).exec();
+      }).skip((page - 1) * limit)
+      .limit(limit).exec();
+  
+      res.send({ product });
+    } catch (err) {
+      res.status(500).send({ error: 'Internal server error' });
+    }
+  });
+  productRouter.get("/filter",async(req,res)=>{
+    const category = req.query.category;
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 6;
+  
+    try {
+      const product = await ProductModel.find({category:category}).skip((page - 1) * limit)
+      .limit(limit).exec();
   
       res.send({ product });
     } catch (err) {
